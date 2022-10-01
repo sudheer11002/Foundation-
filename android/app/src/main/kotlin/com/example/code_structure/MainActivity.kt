1e20d2a4fcc134a8730fa608b9ae11d4bc071747
Sudheer <sudheer11002@gmail.com>
2022-10-01 11:57:03 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/code_structure/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.code_structure
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/consts/app_size.dart
@@ -0,0 +1,4 @@
+class AppSize {
+  static const double PAGE_PADDING = 16.0;
+  static const double CARD_RADIUS = 10.0;
+}
